346c6894261aa707785500532907d24dee02e9da
Jan-Torge Schindler <schindler@mpia.de>
2019-08-19 15:01:59 +0200

    solved bug in s1d_fitsplot

--- scripts/s1d_fitsplot.py
+++ scripts/s1d_fitsplot.py
@@ -11,8 +11,11 @@ def parse_arguments():
             """,
         formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument('-f', '--filename', required=True, type=str,
+    parser.add_argument('filename', type=str,
                         help='Filename of spectrum to plot')
+
+    # parser.add_argument('-f', '--filename', required=True, type=str,
+                        # help='Filename of spectrum to plot')
 
     parser.add_argument('-s', '--smooth', required=False, type=float,
                         help='Number of pixels for a simple boxcar smoothing '
@@ -31,21 +34,22 @@ if __name__ == '__main__':
     hdu = fits.open(args.filename)
 
     try:
-        if 'OPT_WAVE' in hdu[1].columns.names:
-            spec.read_pypeit_fits(filename=args.filename)
+        if len(hdu) > 1:
+            if 'OPT_WAVE' in hdu[1].columns.names:
+                spec.read_pypeit_fits(filename=args.filename)
 
-            if args.save:
-                filename = args.filename.split('.')[0]+'.csv'
-                print(filename)
-                spec.save_to_csv(filename,format='linetools')
+                if args.save:
+                    filename = args.filename.split('.')[0]+'.csv'
+                    print(filename)
+                    spec.save_to_csv(filename,format='linetools')
 
-            if args.smooth is not None:
-                spec.smooth(args.smooth, inplace=True)
+                if args.smooth is not None:
+                    spec.smooth(args.smooth, inplace=True)
 
-            spec.sigmaclip_flux(low=4, up=4, inplace=True)
-            spec.pypeit_plot(show_flux_err=True)
+                spec.sigmaclip_flux(low=4, up=4, inplace=True)
+                spec.pypeit_plot(show_flux_err=True)
 
-        else:
+        elif len(hdu) > 1 or len(hdu) == 1:
             spec.read_from_fits(filename=args.filename)
 
             if args.save:
